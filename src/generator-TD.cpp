30cecc5f38fd6a7662cb0f0de1bc38099bbc791f
MartinaCerulli <72506619+MartinaCerulli@users.noreply.github.com>
2022-07-15 19:16:31 +0200

    Update generator-TD.cpp

--- src/generator-TD.cpp
+++ src/generator-TD.cpp
@@ -4,8 +4,19 @@ written by Mercedes Pelegrín and Martina Cerulli.
 ---------------------------------------------------------------------------------------
 This code is licensed under the MIT license.
 Copyright <2021> <Ecole Polytechnique - IP Paris>
-See file LICENSE at the root of the git project for other information about the copyright notice and 
-the permission notice that shall be included in all copies or substantial portions of this code.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
+(the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge,
+publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 -----------------------------------------------------------------------------------------*/
 #include <stdio.h>
 #include <fstream>
